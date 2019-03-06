3c80f995a9671a06f5b7efd4287c5a84d0602e0d
Megan Jones <38929577+meginks@users.noreply.github.com>
2019-03-06 14:57:33 -0600

    created react app and friends component

---
+++ friends/src/components/Friends.js
@@ -0,0 +1,12 @@
+import React from 'react'; 
+
+class Friends extends React.Component {
+    render(
+            <div>
+            This is a Test 
+            </div>
+    )
+}
+
+
+export default Friends; 
